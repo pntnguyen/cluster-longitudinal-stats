688311740de3ddb2a906a5889474f77449fa2dec
The Nguyen <143731553+pntnguyen@users.noreply.github.com>
2025-12-14 11:25:20 +0700

    lme

--- note.R
+++ note.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(stringr)
-install.packages("mice")
+# install.packages("mice")
 
 library(mice)
 
@@ -107,3 +107,111 @@ for (g in clusters) {
 }
 
 bread %*% meat %*% bread
+
+appendix <- appendix |> 
+  mutate(lognchanges = log(nchanges),
+         group = factor(group,labels = c("Lap","Open")))  
+
+  ggplot(aes(lognchanges))+
+  geom_histogram()
+
+appendix |> 
+  ggplot(aes(x = day, y = lognchanges))+
+  geom_line(aes(group = patid))+
+  geom_point()+
+  facet_wrap(~group)+
+  theme_bw()
+
+## lmer model
+
+### random intercept model
+library(lme4)
+
+data <- potthoffroy |> 
+  pivot_longer(cols = -c(id,sex)) |> 
+  mutate(age = str_extract(name,'\\d+') |> as.numeric(),
+         sex = factor(sex,labels = c(0,1)),
+         time = age-8,
+         y = value) 
+
+
+random_intecept_model <- lmer(y ~ time + (1 | id),data = data, subset = (sex == "1"))
+
+summary(random_intecept_model)
+
+data |> 
+  filter(sex == 1) |> 
+  mutate(fitted = predict(random_intecept_model)) |> 
+  ggplot(aes(x = time, y = fitted))+
+  geom_line(aes(group = id))+
+  geom_point(aes(y = value))
+
+
+## random slope and intecept
+
+random_slope_intecept_model <- lmer(y ~ time + (1 + time | id),data = data, 
+                                    subset = (sex == "Male"), na.action=na.exclude,REML=T)
+
+summary(random_slope_intecept_model)
+
+data |> 
+  filter(sex == 1) |> 
+  mutate(fitted = predict(random_slope_intecept_model)) |> 
+  ggplot(aes(x = time, y = fitted))+
+  geom_line(aes(group = id))+
+  geom_point(aes(y = value))
+
+library(gtsummary)
+
+lmer_gt_tbl <- function(mod){
+  
+ var_s <<- as.data.frame(VarCorr(mod))$vcov[1]
+ var_re <<- as.data.frame(VarCorr(mod))$vcov[length(as.data.frame(VarCorr(mod))$vcov)]
+ 
+  mod |> 
+    tbl_regression(
+      intercept = TRUE,
+      tidy_fun = broom.mixed::tidy,
+      estimate_fun = purrr::partial(style_ratio, digits = 3)
+    ) |>
+    as_gt() |>
+    gt::grand_summary_rows(
+      columns = everything(),
+      fns = list(
+        "Variance (subject)"  = ~ sprintf("%.3f", var_s),
+        "Variance (residual)" = ~ sprintf("%.3f", var_re)
+      )
+    )  
+}
+
+lmer_gt_tbl(random_intecept_model)
+
+lmer_gt_tbl(random_slope_intecept_model)
+
+
+as.data.frame(VarCorr(random_intecept_model))$vcov
+
+as.data.frame(VarCorr(random_slope_intecept_model))$vcov |> length()
+
+
+random_slope_intecept_model2 <- lmer(y ~ sex*time + (1 + time | id),data = data)
+
+summary(random_slope_intecept_model2)
+  
+data |> 
+  mutate(fitted = predict(random_slope_intecept_model2),
+         sex2 = factor(sex) |> as.numeric(),
+         pred.pop = predict(
+           random_slope_intecept_model2,
+           re.form = NA   # fixed effects only
+         )) |> 
+  ggplot(aes(x = time))+
+  geom_line(aes(y = pred.pop,group = id),size = 1)+
+  # geom_line(aes(y = fitted,group = id),alpha = .5)+
+  geom_line(aes(y = value,group = id),alpha = .5)+
+  geom_point(aes(y = value))+
+  facet_wrap(~sex)
+
+
+
+
